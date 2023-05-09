3503d8da5e8839c34c2977b77319bb35222db2f0
EC2 Default User <ec2-user@ip-172-31-85-28.ec2.internal>
2023-05-09 17:11:10 +0000

    New changes to run.py to call app.config

--- web/run.py
+++ web/run.py
@@ -8,17 +8,31 @@
 ##
 __author__ = 'Vas Vasiliadis <vas@uchicago.edu>'
 
-import sys
-import time
+import requests
 import subprocess
-import boto3
+import json
+import os
+import shutil
+import uuid
 from datetime import datetime
-# adding anntools to the system path
-sys.path.insert(0, '/home/ec2-user/mpcs-cc/anntools')
-import driver
+import boto3
+from boto3.dynamodb.conditions import Key
+from botocore.client import Config
+from botocore.exceptions import ClientError
 
-dynamo = boto3.resource('dynamodb', region_name = "us-east-1")
-table = dynamo.Table('idalina_annotations')
+from flask import (abort, flash, redirect, render_template,
+  request, session, url_for)
+
+from gas import app, db
+from decorators import authenticated, is_premium
+from auth import get_profile, update_profile
+# # adding anntools to the system path
+# sys.path.insert(0, '/home/ec2-user/mpcs-cc/anntools')
+# import driver
+
+dynamo = boto3.resource('dynamodb', region_name = app.config["AWS_REGION_NAME"])
+table = dynamo.Table(app.config['AWS_DYNAMODB_ANNOTATIONS_TABLE'])
+results_bucket = app.config["AWS_S3_RESULTS_BUCKET"]
 
 """A rudimentary timer for coarse-grained profiling
 """
@@ -42,6 +56,8 @@ if __name__ == '__main__':
             driver.run(sys.argv[1], "vcf")
             # create job unique id
             job_id = sys.argv[1].split("/")[1]
+            # extract user id
+            user_id = session['primary_identity']
             # grab file name without full path
             file_name = sys.argv[1].split("/")[2]
             # grab annotator log file with and without job id
@@ -51,20 +67,19 @@ if __name__ == '__main__':
             result_file_name = f"{file_name_wo_vcf}.annot.vcf"
             result_file_path = f"output/{job_id}/{result_file_name}"
             # Upload the log and result file
-            # source: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
             s3 = boto3.client('s3')
+            # extract prefix
+            prefix = app.config['AWS_S3_KEY_PREFIX'] + user_id
 
             with open(log_file, "rb") as f:
-                s3.upload_fileobj(f, "mpcs-cc-gas-results", f"idalina/{file_name}.count.log")
+                s3.upload_fileobj(f, results_bucket, f"{prefix}/{file_name}.count.log")
             with open(result_file_path, "rb") as f:
-                s3.upload_fileobj(f, "mpcs-cc-gas-results", f"idalina/{result_file_name}")
+                s3.upload_fileobj(f, results_bucket, f"{prefix}/{result_file_name}")
 
             # add the name of the S3 key for the results file
             # Adds the name of the S3 key for the log file
             # Adds the completion time (use the current system time)
             # Updates the “job_status” key to “COMPLETED”
-            # datetime object containing current date and time
-            # source: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb/client/update_item.html
             now = datetime.now()
             dt_string = now.strftime("%d%m%Y%H%M%S")
             r2 = table.update_item(
