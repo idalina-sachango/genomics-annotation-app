fbee3aedc818976113e35b1bf0f80319b6f6c3e9
EC2 Default User <ec2-user@ip-172-31-88-91.ec2.internal>
2023-05-15 04:37:01 +0000

    Better inittial commit for archiving

--- util/archive/archive.py
+++ util/archive/archive.py
@@ -9,17 +9,18 @@ __author__ = 'Vas Vasiliadis <vas@uchicago.edu>'
 
 import os
 import sys
+import boto3
 
 # Import utility helpers
 sys.path.insert(1, os.path.realpath(os.path.pardir))
 import helpers
 
 # Get configuration
-from configparser import SafeConfigParser
-config = SafeConfigParser(os.environ)
-config.read('notify_config.ini')
+from configparser import ConfigParser
+config = ConfigParser(os.environ)
+config.read('archive_config.ini')
 
-db_name = config["dynamodb"]["TableName"]
+db_name = config["postgres"]["TableName"]
 results_bucket_name = config["s3"]["ResultsBucket"]
 prefix = config["s3"]["PrefixS3"]
 
@@ -27,17 +28,19 @@ prefix = config["s3"]["PrefixS3"]
 
 #Then use the session to get the resource
 s3 = boto3.client('s3')
-result = client.list_objects(Bucket=results_bucket_name, Prefix=prefix, Delimiter='/')
+result = s3.list_objects(Bucket=results_bucket_name, Prefix=prefix, Delimiter='/')
 for o in result.get('CommonPrefixes'):
-    print(o)
-    # print 'sub folder : ', o.get('Prefix')
+    user_file_path = o.get('Prefix')
+    # select all free users from s3
+    user_id = user_file_path.split("/")[1]
+    user_profile = helpers.get_user_profile(id=user_id, db_name=db_name)
+    user_type = [x for x in user_profile if x == "free_user"]
+    print(user_type)
 
-for objects in my_bucket.objects.filter(Prefix="csv_files/"):
-    print(objects.key)
+# for objects in my_bucket.objects.filter(Prefix="csv_files/"):
+#     print(objects.key)
 
-# select all free users from s3
-user_profile = helpers.get_user_profile(job_id=None, db_name=db_name)
-user_type = [x for x in user_profile if x == "free" or x == "premium"]
+
 
 # download(?) file from s3
 
