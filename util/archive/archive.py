812bb3cbe3bca408167b76c9e54a703111ea8ef3
EC2 Default User <ec2-user@ip-172-31-88-91.ec2.internal>
2023-05-15 08:17:45 +0000

    Archived files after 5 minutes

--- util/archive/archive.py
+++ util/archive/archive.py
@@ -23,10 +23,13 @@ config = ConfigParser(os.environ)
 config.read('archive_config.ini')
 
 db_name = config["postgres"]["TableName"]
+dynamo_name = config["dynamo"]["TableName"]
 results_bucket_name = config["s3"]["ResultsBucket"]
 prefix = config["s3"]["PrefixS3"]
 region_name = config["aws"]["AwsRegionName"]
 
+dynamo = boto3.resource('dynamodb', region_name = region_name)
+table = dynamo.Table(dynamo_name)
 
 # Add utility code here
 if "output" not in os.listdir("./"):
@@ -40,49 +43,55 @@ while True:
         os.mkdir("./output")
     messages = queue.receive_messages(WaitTimeSeconds=10)
     for message in messages:
-        body = json.loads(message.body)
-        # extract message
-        messge = json.loads(body["Message"])
-        print("INCOMING MESSAGE\n\n",messge,"\n")
-        # extract job id
-        job_id = messge["job_id"]
-        # extract user id
-        user_id = messge['user_id']
+        try: 
+            body = json.loads(message.body)
+            # extract message
+            messge = json.loads(body["Message"])
+            print("INCOMING MESSAGE\n\n",messge,"\n")
+            # extract job id
+            job_id = messge["job_id"]
+            # extract user id
+            user_id = messge['user_id']
 
-        bucket_file_path = prefix + user_id
+            bucket_file_path = prefix + user_id
 
-        # os.makedirs(f"output/{user_id}")
-        # s3.download_file(
-        #     config["s3"]["ResultsBucket"],
-        #     bucket_file_path,
-        #     f"output/{user_id}"
-        # )
-        s3 = boto3.client('s3')
-        bucket = s3.list_objects(Bucket= results_bucket_name, Prefix=prefix)['Contents']
-
-        for s3_object in bucket: 
-            bucket_file_path = s3_object['Key']
-            if "vcf" in bucket_file_path or "log" in bucket_file_path:
-                file_name = s3_object['Key'].split("/")[2]
-                print(file_name)
-                print(bucket_file_path)
-                try:
+            s3 = boto3.client('s3')
+            bucket = s3.list_objects(Bucket= results_bucket_name, Prefix=prefix)['Contents']
+            for s3_object in bucket: 
+                bucket_file_path = s3_object['Key']
+                if "annot.vcf" in bucket_file_path:
+                    file_name = s3_object['Key'].split("/")[2]
                     s3.download_file(
                         results_bucket_name, 
                         bucket_file_path, 
                         f"output/{file_name}"
                     )
+                    glacier = boto3.client('glacier')
+                    response = glacier.upload_archive(
+                        vaultName=config["glacier"]["VaultName"],
+                        body=f"output/{file_name}"
+                    )
+                    archive_id = response["ResponseMetadata"]["HTTPHeaders"]["x-amzn-requestid"]
+                    response_db = table.update_item(
+                        TableName=dynamo_name,
+                        Key={'job_id': job_id},
+                        UpdateExpression='set results_file_archive_id = :r',
+                        ExpressionAttributeValues={
+                            ':r': str(archive_id)
+                        },
+                        ReturnValues='UPDATED_NEW'
+                    )
+                    s3.delete_object(
+                        Bucket=results_bucket_name,
+                        Key=bucket_file_path
+                    )
+        except:
+            raise
+        finally:
+            message.delete()
+            shutil.rmtree("output")
 
-                    response = client.upload_archive(
-                        vaultName='string',
-                        archiveDescription='string',
-                        body=b'bytes'|file
-                    )   
-                except:
-                    raise
-    shutil.rmtree("output")
-
-print("done!")
+    print("done!\n\n")
 
 
 
