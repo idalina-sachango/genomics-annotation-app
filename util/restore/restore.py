7eb82052ae2406b5b43ad3040439850189f84101
EC2 Default User <ec2-user@ip-172-31-88-91.ec2.internal>
2023-05-24 05:28:27 +0000

    Restore process

--- util/restore/restore.py
+++ util/restore/restore.py
@@ -47,11 +47,8 @@ while True:
             glacier = boto3.client('glacier')
             for job in job_list:
                 if "results_file_archive_id" in job.keys():
-                    print("archive id=", job["results_file_archive_id"])
                     if len(job["results_file_archive_id"]) > 0:
-                        print(job["results_file_archive_id"])
                         try:
-                            print("TRYING EXPEDITED RETRIEVAL")
                             # try expedited
                             response = glacier.initiate_job(
                                 vaultName = config["glacier"]["VaultName"],
@@ -62,15 +59,7 @@ while True:
                                     "SNSTopic": "arn:aws:sns:us-east-1:659248683008:idalina_thaw"
                                 },
                             )
-                            # send to thaw message queue
-                            job_id = response["jobId"]
-                            mess = {
-                                "job_id": job_id
-                            }
-
-                            sns_send_thaw(str(json.dumps(mess)))
                         except:
-                            print("SWITCHING TO STANDARD RETRIEVAL")
                             # try standard if expedited didnt work
                             response = glacier.initiate_job(
                                 vaultName = config["glacier"]["VaultName"],
@@ -81,13 +70,6 @@ while True:
                                     "SNSTopic": "arn:aws:sns:us-east-1:659248683008:idalina_thaw"
                                 },
                             )
-                            # send to thaw message queue
-                            job_id = response["jobId"]
-                            mess = {
-                                "job_id": job_id
-                            }
-
-                            sns_send_thaw(str(json.dumps(mess)))
             print("done processing job list!\n\n")
         except:
             raise
--- util/thaw/thaw.py
+++ util/thaw/thaw.py
@@ -11,6 +11,7 @@ import os
 import sys
 import boto3
 import json
+from boto3.dynamodb.conditions import Key
 
 # Import utility helpers
 sys.path.insert(1, os.path.realpath(os.path.pardir))
@@ -23,6 +24,7 @@ config.read('thaw_config.ini')
 
 
 # Add utility code here
+results_bucket = config["s3"]["ResultsBucket"]
 
 dynamo_name = config["dynamo"]["TableName"]
 region_name = config["aws"]["AwsRegionName"]
@@ -40,26 +42,30 @@ while True:
         body = json.loads(message.body)
         # extract message
         messge = json.loads(body["Message"])
-        if "JobId" in messge.keys():
-            job_id = messge["JobId"]
-            glacier = boto3.client('glacier')
+        glacier = boto3.client('glacier')
+        if "Completed" in messge.keys() and messge["Completed"]:
             try:
-                # status = glacier.describe_job(
-                #     vaultName=config["glacier"]["VaultName"],
-                #     jobId=job_id
-                # )
-                # if status["Completed"]:
-                #     print("Completed!")
-                response = glacier.get_job_output(
+                status = glacier.describe_job(
                     vaultName=config["glacier"]["VaultName"],
-                    jobId=job_id
+                    jobId=messge["JobId"]
                 )
-                file_body = response["body"]
-                print(file_body)
-                print(file_body.read())
-            except:
-                print("Job not in glacier vault. Deleting message")
-                # message.delete()
+                if status["Completed"]:
+                    response = glacier.get_job_output(
+                        vaultName=config["glacier"]["VaultName"],
+                        jobId=messge["JobId"]
+                    )
+                    file_body = response["body"]
+                    print(file_body.read())
+                    print(json.loads(file_body.read()))
+                    file_param = file_body.read()
+                    prefix = config["s3"]["PrefixS3"]
+                    result_file_name = response["Items"][0]["s3_key_result_file"]
+                    s3 = boto3.client('s3')
+                    s3.upload_fileobj(file_param, results_bucket, f"{prefix}/{result_file_name}")
+            except Exception as err:
+                print("Job id not in glacier")
+                message.delete()
+        
     print("done!\n\n")         
 
 ### EOF
