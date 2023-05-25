8c2d3455a6f920023628e3c33d8e18922f9da831
EC2 Default User <ec2-user@ip-172-31-88-91.ec2.internal>
2023-05-25 02:32:45 +0000

    Fixed bug in archive that wasn't archiving all files

--- util/archive/archive.py
+++ util/archive/archive.py
@@ -11,6 +11,8 @@ import sys
 import boto3
 import json
 import shutil
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
 
 # Import utility helpers
 sys.path.insert(1, os.path.realpath(os.path.pardir))
@@ -37,61 +39,64 @@ queue = boto3.resource("sqs", region_name=region_name).Queue(url)
 while True:
     messages = queue.receive_messages(WaitTimeSeconds=10)
     for message in messages:
-        if "output" not in os.listdir("./"):
-            os.mkdir("./output")
         try: 
             body = json.loads(message.body)
             # extract message
             messge = json.loads(body["Message"])
             # extract job id
             job_id = messge["job_id"]
+            # write output to jobs own directory
+            os.makedirs(f"./output/{job_id}")
             # extract user id
             user_id = messge['user_id']
             bucket_file_path = prefix + user_id
             s3 = boto3.client('s3')
-            # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/list_objects.html
-            bucket = s3.list_objects(Bucket= results_bucket_name, Prefix=prefix)['Contents']
-            for s3_object in bucket: 
-                bucket_file_path = s3_object['Key']
-                if "annot.vcf" in bucket_file_path:
-                    file_name = s3_object['Key'].split("/")[2]
-                    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-example-download-file.html
-                    s3.download_file(
-                        results_bucket_name, 
-                        bucket_file_path, 
-                        f"output/{file_name}"
-                    )
-                    glacier = boto3.client('glacier')
-                    # Read the file into a seekable file-like object
-                    with open(f"output/{file_name}", 'rb') as file:
-                        file_contents = file.read()
-                        seekable_file = io.BytesIO(file_contents)
-                        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/glacier/client/upload_archive.html
-                        response = glacier.upload_archive(
-                            vaultName=config["glacier"]["VaultName"],
-                            body=seekable_file
-                        )
-                    archive_id = response["archiveId"]
-                    response_db = table.update_item(
-                        TableName=dynamo_name,
-                        Key={'job_id': job_id},
-                        UpdateExpression='set results_file_archive_id = :r',
-                        ExpressionAttributeValues={
-                            ':r': str(archive_id)
-                        },
-                        ReturnValues='UPDATED_NEW'
-                    )
-                    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/delete_object.html
-                    s3.delete_object(
-                        Bucket=results_bucket_name,
-                        Key=bucket_file_path
-                    )
+            # get result file name and path 
+            quer = response = table.query(
+                KeyConditionExpression=Key("job_id").eq(job_id)
+            )
+            result_name = quer["Items"][0]["s3_key_result_file"]
+            result_user_id_path = f"{user_id}/{result_name}"
+            bucket_file_path = prefix + result_user_id_path
+            # download reuslt file
+            # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-example-download-file.html
+            s3.download_file(
+                results_bucket_name, 
+                bucket_file_path, 
+                f"output/{job_id}/{result_name}"
+            )
+            # upload to glacier
+            glacier = boto3.client('glacier')
+            # Read the file into a seekable file-like object
+            with open(f"output/{job_id}/{result_name}", 'rb') as file:
+                file_contents = file.read()
+                seekable_file = io.BytesIO(file_contents)
+                # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/glacier/client/upload_archive.html
+                response = glacier.upload_archive(
+                    vaultName=config["glacier"]["VaultName"],
+                    body=seekable_file
+                )
+            # update dynamo db
+            archive_id = response["archiveId"]
+            response_db = table.update_item(
+                TableName=dynamo_name,
+                Key={'job_id': job_id},
+                UpdateExpression='set results_file_archive_id = :r',
+                ExpressionAttributeValues={
+                    ':r': str(archive_id)
+                },
+                ReturnValues='UPDATED_NEW'
+            )
+            # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/delete_object.html
+            s3.delete_object(
+                Bucket=results_bucket_name,
+                Key=bucket_file_path
+            )      
         except:
             raise
         finally:
             message.delete()
-            shutil.rmtree("output")
-
+            shutil.rmtree(f"./output/{job_id}")
     print("done!\n\n")
 
 ### EOF
